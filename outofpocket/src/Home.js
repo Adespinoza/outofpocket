c3a6857ba62742297944871422da719a87445015
ijemmao <ijemmaonwuzulike10@gmail.com>
2017-07-15 21:51:17 -0700

    Able to grab the text from the textarea and pass

--- outofpocket/src/Home.js
+++ outofpocket/src/Home.js
@@ -5,7 +5,6 @@ import {
   Link
 } from 'react-router-dom'
 import './Home.css';
-// import Results from './components/Results/index.js'
 let placeholder = '';
 const randomPhrases = [
   "I'm not racist, but I think affirmative action is reverse racism.",
@@ -15,6 +14,26 @@ const randomPhrases = [
 ]
 
 class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    // Sends the phrase to the backend
+    // Jumps to the results page
+    console.log(this.state.value);
+  }
+
   randomPlaceholder = () => {
     placeholder = randomPhrases[Math.floor(Math.random() * (randomPhrases.length))];
   }
@@ -28,10 +47,10 @@ class Home extends Component {
           <h6>A problematic language processing program</h6>
         </div>
         <div className="container--fluid column center-container phrase-container">
-          <h1 class="phrase-header">Enter Sentence or Phrase</h1>
-          <textarea rows="10" cols="50" className="phrase-area" placeholder={placeholder}>
+          <h1 className="phrase-header">Enter Sentence or Phrase</h1>
+          <textarea rows="10" cols="50" className="phrase-area" placeholder={placeholder} value={this.state.value} onChange={this.handleChange}>
           </textarea>
-          <button className="primary btn-large">Submit</button>
+          <button type="submit" className="primary btn-large" onClick={this.handleSubmit}>Submit</button>
         </div>
       </div>
       </Router>
