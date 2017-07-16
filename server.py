a0bad8b9141a6fa6f5bf340bc569ff192a3c7947
Ebube Chuba <ebube97@me.com>
2017-07-16 05:45:17 -0700

    last minute server side fixes 😅

--- server.py
+++ server.py
@@ -5,25 +5,26 @@ import tempfile
 
 app = FlaskAPI(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST', 'OPTIONS'])
 def process():
-  text = request.data.get('text')
-  print(text)
-  temp = tempfile.NamedTemporaryFile()
-  temp.write(bytes(text, 'UTF-8'))
-  temp.seek(0)
-  model_out = subprocess.check_output(['./fasttext', 'predict-prob', 'out_of_pocket.bin', temp.name, '12']).decode('UTF-8')
-  label = True
-  last_label = ''
   resp = {}
+  if request.method == 'POST':
+    text = request.data.get('text')
+    print(text)
+    temp = tempfile.NamedTemporaryFile()
+    temp.write(bytes(text, 'UTF-8'))
+    temp.seek(0)
+    model_out = subprocess.check_output(['./fasttext', 'predict-prob', 'out_of_pocket.bin', temp.name, '12']).decode('UTF-8')
+    label = True
+    last_label = ''
 
-  for v in model_out.split():
-    if label:
-      last_label = v
-    else:
-      resp[last_label] = float(v)
-    label = not label
-  return resp, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'POST', 'Access-Control-Allow-Headers': 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With', 'Content-Type': 'application/json'}
+    for v in model_out.split():
+      if label:
+        last_label = v
+      else:
+        resp[last_label] = float(v)
+      label = not label
+  return resp, {'Access-Control-Allow-Origin': 'http://localhost:3000', 'Access-Control-Allow-Methods': 'POST, OPTIONS, GET', 'Access-Control-Allow-Headers': 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With', 'Content-Type': 'application/json'}
 
 
 if __name__ == "__main__":
