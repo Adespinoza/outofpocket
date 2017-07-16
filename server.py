abc2ba0951774da8c3dc09daef5abfa6903136dc
Ebube Chuba <ebube97@me.com>
2017-07-16 08:05:44 -0700

    yiiiikes alright maybe now?

--- server.py
+++ server.py
@@ -30,19 +30,20 @@ def process():
       resp['__label__xeno'] = 0.12
       resp['__label__homo'] = 0.01
       resp['__label__sexi'] = 0.08
-    temp = tempfile.NamedTemporaryFile()
-    temp.write(bytes(text, 'UTF-8'))
-    temp.seek(0)
-    model_out = subprocess.check_output(['./fasttext', 'predict-prob', 'out_of_pocket.bin', temp.name, '12']).decode('UTF-8')
-    label = True
-    last_label = ''
+    else:
+      temp = tempfile.NamedTemporaryFile()
+      temp.write(bytes(text, 'UTF-8'))
+      temp.seek(0)
+      model_out = subprocess.check_output(['./fasttext', 'predict-prob', 'out_of_pocket.bin', temp.name, '12']).decode('UTF-8')
+      label = True
+      last_label = ''
 
-    for v in model_out.split():
-      if label:
-        last_label = v
-      else:
-        resp[last_label] = float(v)
-      label = not label
+      for v in model_out.split():
+        if label:
+          last_label = v
+        else:
+          resp[last_label] = float(v)
+        label = not label
   return resp, {'Access-Control-Allow-Origin': 'http://localhost:3000', 'Access-Control-Allow-Methods': 'POST, OPTIONS, GET', 'Access-Control-Allow-Headers': 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With', 'Content-Type': 'application/json'}
 
 
