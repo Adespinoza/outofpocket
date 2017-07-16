6424e72b743c712c25459fa2df5a62cdf00cae71
christtina <christina.vhoward@gmail.com>
2017-07-16 02:55:54 -0700

    preprocessing datat

--- preprocess.py
+++ preprocess.py
@@ -1,10 +1,13 @@
 import csv
 
-data = open("problematic_phrases.csv", "r")
-output = open("yikes.train", "w")
+
+data = open("problematic_text.csv", "r")
+
+output = open("yikes.csv", "w")
 reader = csv.DictReader(data, fieldnames=["theme", "text"])
 
 # TODO: create 80-20 train/test split
 
 for row in reader:
-  output.write("__label__{} {}\n".format(row["theme"].replace(' ', '_'), row["text"].lower().strip(",.?")))
+  output.write("__label__{} {}\n".format(row["theme"].replace(' | ', '_'), row["text"].lower().strip("(@[A-Za-z0-9]+)|([^0-9A-Za-z])|(\w+:\/\/\S+)")))
+
